bd61aebeb534dc5d557529bb34a832be489ddf25
mrcal56 <marlon56r@gmail.com>
2024-07-11 16:17:28 -0600

    Redux and PokeApi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {
+  useEffect
+} from 'react';
+import {
+  Col,
+  Spin
+} from 'antd';
+import {
+  shallowEqual,
+  useDispatch,
+  useSelector
+} from 'react-redux';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import logo from './statics/logo.svg';
+import {
+  fetchPokemonsWithDetails
+} from './slices/dataSlice';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  const pokemons = useSelector((state) => state.data.pokemons, shallowEqual);
+  const loading = useSelector((state) => state.ui.loading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPokemonsWithDetails());
+  }, []);
+  return ( 
+  <div className = 'App'> 
+  <Col span = {4}
+  offset = { 10}> 
+  <img src = {logo }
+  alt = 'Pokedux' />
+  </Col>
+  <Col span={8}offset={8}>
+  <Searcher/> 
+  </Col>{loading?(<Col offset={12}>
+  <Spin spinning size='large'/> 
+  </Col>):(<PokemonList pokemons={pokemons}/> )}
+   </div>);}
 export default App;
